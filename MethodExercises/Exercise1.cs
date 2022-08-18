ece542a6f35094d33ff1d0ca2c28e606ac0a08df
Matteo DeSimone <thematteod@gmail.com>
2022-08-18 16:08:34 -0400

    Exercise 1 done

---
+++ MethodExercises/Exercise1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MethodExercises
+{
+    internal class Exercise1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I have a story to tell!");
+            Console.WriteLine("But first I need your help, could you tell me a little bit about yourself?");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Console.ReadLine();
+            Console.WriteLine("Just one more question, I promise! What is your favorite band or musical artist?");
+            string band = Console.ReadLine();
+            Console.WriteLine("Okay, here we go!");
+            Console.WriteLine($"When {name} was a young child they would put on their favorite {color} shirt and take their pet {animal} for a walk while listening to {band} on their Walkman Cd Player!");
+        }
+    }
+}
