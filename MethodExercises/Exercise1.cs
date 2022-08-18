bd2d48b6f528a1c15138c783dd9a41c5579eb793
Matteo DeSimone <thematteod@gmail.com>
2022-08-18 16:57:44 -0400

    Exercise 2 done

--- MethodExercises/Exercise1.cs
+++ MethodExercises/Exercise1.cs
@@ -2,7 +2,7 @@
 
 namespace MethodExercises
 {
-    internal class Exercise1
+    internal class Program
     {
         static void Main(string[] args)
         {
@@ -14,7 +14,7 @@ namespace MethodExercises
             string color = Console.ReadLine();
             Console.WriteLine("What is your favorite animal?");
             string animal = Console.ReadLine();
-            Console.WriteLine("Just one more question, I promise! What is your favorite band or musical artist?");
+            Console.WriteLine("Just one more question, I promise! What is your favorite band?");
             string band = Console.ReadLine();
             Console.WriteLine("Okay, here we go!");
             Console.WriteLine($"When {name} was a young child they would put on their favorite {color} shirt and take their pet {animal} for a walk while listening to {band} on their Walkman Cd Player!");
---
+++ MethodExercises/Exercise2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MethodExercises
+{
+    internal class Exercise2
+    {
+        public static int AddDigits(params int[] values)
+        {
+            int answer = values.Sum();
+            return answer;
+        }
+
+        public static int MultiplyDigits(int num1, int num2)
+        {
+            int answer = num1 * num2;
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddDigits(1, 2, 3, 4, 5, 6, 7));
+            Console.WriteLine(MultiplyDigits(5, 0));
+
+        }
+    }
+}
